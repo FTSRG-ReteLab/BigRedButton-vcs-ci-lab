9d063025aee8703c4bf14695aa92e5494ef3318b
fandras95 <fandras95@gmail.com>
2018-03-12 07:56:45 -0700

    Add AlarmService

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -23,6 +23,16 @@ public class TrainSensorImpl implements TrainSensor {
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
+
+		if (!user.getAlarmState()) {
+			if (speedLimit < 0 || speedLimit > 500) {
+				user.setAlarmState(true);
+			}
+			if (speedLimit/controller.getReferenceSpeed() > 0.5) {
+				user.setAlarmState(true);
+			}
+		}
+
 		controller.setSpeedLimit(speedLimit);
 	}
 
